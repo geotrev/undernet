790ac1cd978189b3bc93782fd2e043b48989efa5
George <gtreviranus@gmail.com>
2017-12-20 16:49:30 -0600

    whitespace adjustment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './Index.scss';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 
 ReactDOM.render(
