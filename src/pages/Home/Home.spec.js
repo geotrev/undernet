ed925de5f02800a30d42c7ddf59413b2d21a0c42
George <gtreviranus@gmail.com>
2017-12-22 14:22:27 -0600

    adds more home tests

--- src/pages/Home/Home.spec.js
+++ src/pages/Home/Home.spec.js
@@ -5,4 +5,14 @@ describe("<Home />", () => {
     const wrapper = shallow(<Home />);
     expect(wrapper).to.have.id('home');
   })
+  
+  it("has one .intro-wrapper (<Intro />)", () => {
+    const wrapper = mount(<Home />);
+    expect(wrapper).to.have.descendants('.intro-wrapper');
+  })
+  
+  it("has one <img> (<FeatureImage />)", () => {
+    const wrapper = mount(<Home />);
+    expect(wrapper).to.have.descendants('img');
+  })
 })
