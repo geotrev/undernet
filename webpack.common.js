e3a8478f1ea3dba3fd7d2fbce09ba877a0ec47fd
George <gtreviranus@gmail.com>
2017-12-25 23:05:37 -0600

    removes surge (for now)

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', "postcss-loader", 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.(jpe?g|png|gif)$/,
