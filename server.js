eb4a665f7ca561ce18f59686ee3cec6f5ac0f81f
George <gtreviranus@gmail.com>
2017-12-27 00:21:58 -0600

    add process.env.port support oops

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./build'));
 
@@ -8,4 +9,5 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
-app.listen(3000);
+
+app.listen(PORT, () => `App running at localhost:${PORT}...`);
