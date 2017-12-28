38528fe51e8b24305095b67c26517a129181ea58
George <gtreviranus@gmail.com>
2017-12-28 05:45:39 -0600

    caps on port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -9,6 +9,6 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
-app.listen(port, error => {
+app.listen(PORT, error => {
   error ? console.log(error) : console.info(`--> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
 });
