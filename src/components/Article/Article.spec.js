bfea49c442f4af079bac5a1d3c935d7285fe449e
George Treviranus <george@scribd.com>
2018-01-09 11:58:31 -0800

    updates test cases

--- src/components/Article/Article.spec.js
+++ src/components/Article/Article.spec.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Article from './Article';
-import Content from 'articles/Buttons.md'
 
-describe("<Article />", () => {
+describe("<Article></Article>", () => {
   it("can render HTML from markdown", () => {
-    // const md = "# Test header \n So neat";
-    const wrapper = mount(<Article>{Content}</Article>);
+    const md = "# Test header \n So neat";
+    const wrapper = mount(<Article>{md}</Article>);
     expect(wrapper).to.have.descendants('h1');
     expect(wrapper).to.have.descendants('p');
   })
