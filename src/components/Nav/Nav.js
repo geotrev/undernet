0c7b07a28fd9b27dbfe5d63263cf88fbe445440c
George <gtreviranus@gmail.com>
2018-01-09 18:29:11 -0800

    scopes styles for components

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,12 +7,12 @@ import { NavLink } from 'react-router-dom';
 export default class Nav extends Component {
   render() {
     return (
-      <nav className="intro-wrapper nav">
-        <ul className="nav-list">
-          <li className="nav-item"> 
+      <nav className="nav-wrapper">
+        <ul>
+          <li> 
             <NavLink activeClassName="selected" exact to='/'>Home</NavLink> 
           </li>
-          <li className="nav-item"> 
+          <li> 
             <NavLink activeClassName="selected" to='/components'>Components</NavLink> 
           </li>
         </ul>
