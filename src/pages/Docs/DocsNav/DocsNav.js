b888f2aebd4264e08f57bf7190a71459bf581a47
George <gtreviranus@gmail.com>
2018-01-14 21:32:21 -0800

    updates styling of mobile menu, removes full-screen-ness of pages in general

--- src/pages/Docs/DocsNav/DocsNav.js
+++ src/pages/Docs/DocsNav/DocsNav.js
@@ -19,10 +19,6 @@ export default class DocsNav extends Component {
   componentWillMount() {
     this.checkWidth();
     window.addEventListener('resize', this.checkWidth);
-    this.setState({
-      menuClasses: 'xsmall-order-2 xsmall-12 columns docs-nav-menu',
-      buttonText: 'Hide Menu'
-    })
   }
 
   componentWillUnmount() {
@@ -30,7 +26,13 @@ export default class DocsNav extends Component {
   }
 
   checkWidth() {
-    if (window.outerWidth > 1199) {
+    if (window.outerWidth < 1200) {
+      this.setState({
+        menuIsOpen: false,
+        menuClasses: 'xsmall-order-2 xsmall-12 columns docs-nav-menu is-not-displayed',
+        buttonText: 'Show Menu'
+      })
+    } else {
       this.setState({
         menuIsOpen: true,
         menuClasses: 'xsmall-order-2 xsmall-12 columns docs-nav-menu',
@@ -54,7 +56,7 @@ export default class DocsNav extends Component {
 
     this.setState({
       menuIsOpen: !this.state.menuIsOpen,
-      menuClasses: `xsmall-order-2 xsmall-12 columns docs-nav-menu ${hideClass}`,
+      menuClasses: `xsmall-order-2 xsmall-12 docs-nav-menu columns ${hideClass}`,
       buttonText: label
     });
   }
@@ -71,30 +73,28 @@ export default class DocsNav extends Component {
           </a>
         </div>
 
-        <div className={this.state.menuClasses}>
-          <nav>
-            <h3 className="paragraph"><strong>Explore Monolith</strong></h3>
-            <h4 className="paragraph">Getting Started</h4>
-            <ul>
-              <li><NavLink activeClassName="selected" to='/docs/overview'>Overview</NavLink></li>
-              <li><NavLink activeClassName="selected" to='/docs/download'>Download</NavLink></li>
-              <li><NavLink activeClassName="selected" to='/docs/configuration'>Configuration</NavLink></li>
-            </ul>
-            <h4 className="paragraph">Components</h4>
-            <ul>
-              <li><NavLink activeClassName="selected" to='/docs/grid'>Grid</NavLink></li>
-              <li><NavLink activeClassName="selected" to='/docs/typography'>Typography</NavLink></li>
-              <li><NavLink activeClassName="selected" to='/docs/buttons'>Buttons</NavLink></li>
-              <li><NavLink activeClassName="selected" to='/docs/forms'>Forms</NavLink></li>
-            </ul>
-            <h4 className="paragraph">Helpers</h4>
-            <ul>
-              <li><NavLink activeClassName="selected" to='/docs/classes'>Classes</NavLink></li>
-              <li><NavLink activeClassName="selected" to='/docs/mixins'>Mixins</NavLink></li>
-              <li><NavLink activeClassName="selected" to='/docs/functions'>Functions</NavLink></li>
-            </ul>
-          </nav>
-        </div>
+        <nav className={this.state.menuClasses}>
+          <h3 className="paragraph"><strong>Explore Monolith</strong></h3>
+          <h4 className="paragraph">Getting Started</h4>
+          <ul>
+            <li><NavLink activeClassName="selected" to='/docs/overview'>Overview</NavLink></li>
+            <li><NavLink activeClassName="selected" to='/docs/download'>Download</NavLink></li>
+            <li><NavLink activeClassName="selected" to='/docs/configuration'>Configuration</NavLink></li>
+          </ul>
+          <h4 className="paragraph">Components</h4>
+          <ul>
+            <li><NavLink activeClassName="selected" to='/docs/grid'>Grid</NavLink></li>
+            <li><NavLink activeClassName="selected" to='/docs/typography'>Typography</NavLink></li>
+            <li><NavLink activeClassName="selected" to='/docs/buttons'>Buttons</NavLink></li>
+            <li><NavLink activeClassName="selected" to='/docs/forms'>Forms</NavLink></li>
+          </ul>
+          <h4 className="paragraph">Helpers</h4>
+          <ul>
+            <li><NavLink activeClassName="selected" to='/docs/classes'>Classes</NavLink></li>
+            <li><NavLink activeClassName="selected" to='/docs/mixins'>Mixins</NavLink></li>
+            <li><NavLink activeClassName="selected" to='/docs/functions'>Functions</NavLink></li>
+          </ul>
+        </nav>
       </div>
     )
   }
