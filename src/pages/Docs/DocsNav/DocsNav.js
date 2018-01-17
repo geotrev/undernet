8b2d888da20e490e8b38a4e33f0e916f76513fb2
George Treviranus <george@scribd.com>
2018-01-17 10:14:37 -0800

    shrinks menu on docs nav link click

--- src/pages/Docs/DocsNav/DocsNav.js
+++ src/pages/Docs/DocsNav/DocsNav.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import './DocsNav.scss';
 
-import { NavLink } from 'react-router-dom';
+import { Button } from 'components'
+import { Link } from 'react-router-dom';
 import { ChevronDown } from 'react-feather';
 
 export default class DocsNav extends Component {
   constructor(props) {
     super(props)
+    
     this.state = {
       menuIsOpen: true,
       menuClasses: '',
       buttonClasses: '',
       buttonText: ''
     }
+    
     this.checkWidth = this.checkWidth.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleCollapseClick = this.handleCollapseClick.bind(this);
   }
 
   componentWillMount() {
@@ -30,6 +34,16 @@ export default class DocsNav extends Component {
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.checkWidth);
+  }
+  
+  handleCollapseClick() {
+    if (window.outerWidth > 1199) return;
+    this.setState({
+      menuIsOpen: false,
+      menuClasses: 'xsmall-order-2 xsmall-12 columns docs-nav-menu is-not-displayed',
+      buttonClasses: 'show-on-xsmall hide-on-xlarge',
+      buttonText: 'Show Menu'
+    })
   }
 
   checkWidth() {
@@ -69,34 +83,34 @@ export default class DocsNav extends Component {
     return (
       <div className="docs-nav-wrapper row">
         <div className="docs-nav-expand show-on-xsmall hide-on-xlarge">
-          <a
+          <Button
             onClick={this.handleClick}
             className={this.state.buttonClasses}
             href="#">
               {this.state.buttonText} <ChevronDown size={20} />
-          </a>
+          </Button>
         </div>
 
         <nav className={this.state.menuClasses}>
           <h3 className="paragraph"><strong>Explore Monolith</strong></h3>
           <h4 className="paragraph">Getting Started</h4>
           <ul>
-            <li><NavLink activeClassName="selected" to='/docs/overview'>Overview</NavLink></li>
-            <li><NavLink activeClassName="selected" to='/docs/download'>Download</NavLink></li>
-            <li><NavLink activeClassName="selected" to='/docs/configuration'>Configuration</NavLink></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/overview'>Overview</Link></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/download'>Download</Link></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/configuration'>Configuration</Link></li>
           </ul>
           <h4 className="paragraph">Components</h4>
           <ul>
-            <li><NavLink activeClassName="selected" to='/docs/grid'>Grid</NavLink></li>
-            <li><NavLink activeClassName="selected" to='/docs/typography'>Typography</NavLink></li>
-            <li><NavLink activeClassName="selected" to='/docs/buttons'>Buttons</NavLink></li>
-            <li><NavLink activeClassName="selected" to='/docs/forms'>Forms</NavLink></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/grid'>Grid</Link></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/typography'>Typography</Link></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/buttons'>Buttons</Link></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/forms'>Forms</Link></li>
           </ul>
           <h4 className="paragraph">Helpers</h4>
           <ul>
-            <li><NavLink activeClassName="selected" to='/docs/classes'>Classes</NavLink></li>
-            <li><NavLink activeClassName="selected" to='/docs/mixins'>Mixins</NavLink></li>
-            <li><NavLink activeClassName="selected" to='/docs/functions'>Functions</NavLink></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/classes'>Classes</Link></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/mixins'>Mixins</Link></li>
+            <li><Link onClick={this.handleCollapseClick} to='/docs/functions'>Functions</Link></li>
           </ul>
         </nav>
       </div>
