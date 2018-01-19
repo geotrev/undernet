3c4670038c385a3061023411fe63d6f96dfa6745
George <gtreviranus@gmail.com>
2018-01-19 08:45:21 -0800

    removes activeClassName from NavLink and prefers Link instead for navigation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,30 +3,33 @@ import './Nav.scss';
 
 import Logo from '../../assets/images/mono-logo.png'
 import { Github, Twitter } from 'react-feather';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav className="nav-wrapper fluid grid">
       <ul className="row collapsed">
         <li className="small-6 xsmall-12 columns">
-          <NavLink exact to='/'><img height="30px" width="auto" src={Logo} alt="Monolith CSS logo"/></NavLink>
+          <Link to='/'>
+            <img height="30px" width="auto" src={Logo} alt="Monolith CSS logo"/>
+          </Link>
         </li>
         <li className="small-6 xsmall-12 columns">
           <ul className="row">
             <li>
               <a className="has-feather" href='https://www.twitter.com/monolithcss'>
                 <Twitter />
-              </a> </li>
+              </a>
+            </li>
             <li>
               <a className="has-feather" href='https://www.github.com/geotrev/monolith'>
                 <Github />
               </a>
             </li>
             <li>
-              <NavLink activeClassName="selected" to='/docs/overview'>
+              <Link to='/docs/overview'>
                 Docs
-              </NavLink>
+              </Link>
             </li>
           </ul>
         </li>
