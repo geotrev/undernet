bed91c6ee9a8d33c74fc03d13def603882f3e804
George <gtreviranus@gmail.com>
2018-01-31 19:23:23 -0800

    unifies layout consistency and removes excess wrappers

--- src/pages/Docs/DocsRoutes/DocsRoutes.js
+++ src/pages/Docs/DocsRoutes/DocsRoutes.js
@@ -11,18 +11,16 @@ const DocsRoutes = () => {
   return (
     <div className="docs-routes-wrapper small-section grid">
       <div className="row">
-        <div className="column">
-          <Route exact path="/docs/overview" component={ Overview } />
-          <Route exact path="/docs/download" component={ Download } />
-          <Route exact path="/docs/configuration" component={ Config } />
-          <Route exact path="/docs/grid" component={ Grid } />
-          <Route exact path="/docs/typography" component={ Type } />
-          <Route exact path="/docs/buttons" component={ Buttons } />
-          <Route exact path="/docs/forms" component={ Forms } />
-          <Route exact path="/docs/classes" component={ Classes } />
-          <Route exact path="/docs/mixins" component={ Mixins } />
-          <Route exact path="/docs/functions" component={ Functions } />
-        </div>
+        <Route exact path="/docs/overview" component={ Overview } />
+        <Route exact path="/docs/download" component={ Download } />
+        <Route exact path="/docs/configuration" component={ Config } />
+        <Route exact path="/docs/grid" component={ Grid } />
+        <Route exact path="/docs/typography" component={ Type } />
+        <Route exact path="/docs/buttons" component={ Buttons } />
+        <Route exact path="/docs/forms" component={ Forms } />
+        <Route exact path="/docs/classes" component={ Classes } />
+        <Route exact path="/docs/mixins" component={ Mixins } />
+        <Route exact path="/docs/functions" component={ Functions } />
       </div>
     </div>
   );
