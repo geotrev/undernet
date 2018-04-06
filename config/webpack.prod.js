a539215a8a9fd4f889b5debf4d178105bb363ef3
George Treviranus <george@scribd.com>
2018-04-06 19:40:19 -0400

    adds manifest and build steps to include it

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,18 +1,35 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require("compression-webpack-plugin")
 const common = require('../webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   plugins: [
-    new CleanWebpackPlugin([ 'build' ], { verbose: true }),
+    new CleanWebpackPlugin(
+      [ '../build' ], 
+      { 
+        root: path.resolve(__dirname),
+        verbose: true,
+        allowExternal: true
+      }
+    ),
     new UglifyJSPlugin({
       sourceMap: true
     }),
     new CompressionPlugin({
       test: /\.js$|.css$/,
       algorithm: "gzip",
+    }),
+    new CopyWebpackPlugin([{ 
+      from: 'public/manifest.json',
+      to: 'manifest.json',
+      toType: 'file',
+      cache: true
+    }], {
+      debug: 'info'
     }),
   ],
   mode: 'production'
