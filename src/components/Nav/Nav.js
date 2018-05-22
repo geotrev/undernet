e89fcb9694654b5419e6b5f56dd4c4e36e3a4091
George <gtreviranus@gmail.com>
2018-05-22 15:31:11 -0700

    updating metadata on tags and github link in nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,13 +12,13 @@ const Nav = () => {
       <ul className="row collapsed">
         <li className="small-5 xsmall-12 columns">
           <Link to="/">
-            <img className="logo" src={Logo} alt="Monolith CSS logo" />
+            <img className="logo" src={Logo} alt="Monolith" />
           </Link>
         </li>
         <li className="small-7 xsmall-12 columns">
           <ul className="row">
             <li>
-              <a className="has-feather" href="https://www.github.com/geotrev/monolith">
+              <a className="has-feather" href="https://www.github.com/geotrev/getmonolith.io">
                 <Github />
               </a>
             </li>
