55a224114777a4a5500757da3141ffd381ce236e
George <gtreviranus@gmail.com>
2018-05-25 21:15:52 -0700

    add class definition

--- dist/js/monolith.js
+++ dist/js/monolith.js
@@ -8,11 +8,18 @@ var _modal = require("./components/modal");
 
 var _modal2 = _interopRequireDefault(_modal);
 
+var _accordion = require("./components/accordion");
+
+var _accordion2 = _interopRequireDefault(_accordion);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Monolith = {
   modals: function modals() {
     return new _modal2.default();
+  },
+  accordions: function accordions() {
+    return new Accordions();
   }
 };
 
@@ -20,10 +27,12 @@ window.Monolith = Monolith || {};
 
 Monolith.start = function () {
   Monolith.modals().start();
+  Monolith.accordions().start();
 };
 
 Monolith.stop = function () {
   Monolith.modals().stop();
+  Monolith.accordions().stop();
 };
 
 exports.default = Monolith;
---
+++ framework/js/components/accordion.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const keyCodes = {}
+const selectors = {}
+const events = {}
+const messages = {}
+
+export default class Accordion {
+  constructor() {}
+  start() {}
+  stop() {}
+}
