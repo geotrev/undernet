b384851799b9b131949a41048b70c4752c2aaa63
George Treviranus <hello@geotrev.com>
2018-08-31 23:08:51 -0700

    add utils to Undernet API availability

--- js/dist/undernet.js
+++ js/dist/undernet.js
@@ -1,33 +1,42 @@
-"use strict";
+"use strict"
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
+  value: true,
+})
+exports.default = void 0
 
-var _modal = _interopRequireDefault(require("./components/modal"));
+var _modal = _interopRequireDefault(require("./components/modal"))
 
-var _accordion = _interopRequireDefault(require("./components/accordion"));
+var _accordion = _interopRequireDefault(require("./components/accordion"))
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _utils = require("./utils")
 
-var Modals = new _modal.default();
-var Accordions = new _accordion.default();
+var _utils2 = _interopRequireDefault(_utils)
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
+
+var Modals = new _modal2.default()
+var Accordions = new _accordion2.default()
+var Utilities = new _utils2.default()
+
 var Undernet = {
   Modals: Modals,
-  Accordions: Accordions
-};
+  Accordions: Accordions,
+  Utilities: Utilities,
+}
 
-Undernet.start = function () {
-  Undernet.Modals.start();
-  Undernet.Accordions.start();
-};
+Undernet.start = function() {
+  Undernet.Modals.start()
+  Undernet.Accordions.start()
+}
 
-Undernet.stop = function () {
-  Undernet.Modals.stop();
-  Undernet.Accordions.stop();
-};
+Undernet.stop = function() {
+  Undernet.Modals.stop()
+  Undernet.Accordions.stop()
+}
 
-window.Undernet = Undernet;
-var _default = Undernet;
-exports.default = _default;
+window.Undernet = Undernet
+var _default = Undernet
+exports.default = _default
