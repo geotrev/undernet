f9b43f27255f298474891c6da12c37ee10f10b8b
George Treviranus <hello@geotrev.com>
2018-10-15 22:42:36 -0700

    new version script links to bin

---
+++ scripts/update.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+const path = require("path")
+const args = require("yargs").argv
+
+const newUndernetVersion = args["tag"]
+const packageFilePath = path.resolve(__dirname, "../package.json")
+const pkg = require(packageFilePath)
+const readFormat = "utf-8"
+
+// Update existing package version to `newUndernetVersion`
+
+function getVersion(version) {
+  return `"version": "${version}"`
+}
+
+const packageVersion = getVersion(pkg.version)
+const newPackageVersion = getVersion(newUndernetVersion)
+const packageFile = fs.readFileSync(packageFilePath, readFormat)
+const newPackageFile = packageFile.replace(packageVersion, newPackageVersion)
+fs.writeFileSync("package.json", newPackageFile, readFormat)
