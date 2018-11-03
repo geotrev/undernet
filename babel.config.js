8456ce4a1c6375521bdc9e277806b220bf10f8a0
George Treviranus <hello@geotrev.com>
2018-11-02 23:30:43 -0700

    rollup --> dist env, no polyfill ever

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-function presets(rollup) {
+function presets(dist) {
   let result = ["@babel/preset-env"]
 
-  if (!rollup) {
+  if (!dist) {
     result.push("@babel/preset-react")
   }
 
@@ -9,22 +9,21 @@ function presets(rollup) {
 }
 
 function plugins(options) {
-  let result = ["@babel/plugin-syntax-dynamic-import", "@babel/plugin-proposal-class-properties"]
+  let result = [
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-proposal-class-properties",
+    ["babel-plugin-webpack-aliases", { config: "config/webpack.dev.js" }],
+  ]
 
   if (options.test) result.push("dynamic-import-node")
-
   if (options.dev || options.prod) result.push("emotion")
-
-  if (options.dev || options.test) {
-    result.push(["babel-plugin-webpack-aliases", { config: "config/webpack.dev.js" }])
-  }
 
   return result
 }
 
 module.exports = {
   env: {
-    rollup: {
+    dist: {
       presets: presets(true),
     },
     development: {
