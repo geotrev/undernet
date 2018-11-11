b3a3e526933531dc34043adce23a61afbc68e8b7
george <george@scribd.com>
2018-11-10 21:53:25 -0800

    add initial modal tests

--- js/test/modal/modal.js
+++ js/test/modal/modal.js
@@ -1,1 +1,44 @@
-// modal component setup
+import React, { Component } from "react"
+import Undernet from "../../src/undernet"
+
+export default class Modal extends Component {
+  constructor() {
+    super()
+  }
+
+  componentWillMount() {
+    Undernet.Modals.start()
+  }
+
+  render() {
+    return (
+      <div>
+        <button href="#" data-modal-button data-target="new-modal">Open modal</button>
+        
+        <div className="modal-overlay" data-modal-id="new-modal">
+          <div className="modal-dialog" data-parent="new-modal" aria-labelledby="header-id" data-modal>
+            <header>
+              <h2 className="h6 has-no-margin-top" id="header-id">
+                Modal Header
+              </h2>
+              <a data-close href="#">
+                <span aria-hidden="true">&times;</span>
+              </a>
+            </header>
+            <section>
+              <p>Some modal content here</p>
+            </section>
+            <footer>
+              <a className="button" data-close href="#">
+                Cancel
+              </a>
+              <a className="primary button" href="#">
+                OK
+              </a>
+            </footer>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- js/test/modal/modal.spec.js
+++ js/test/modal/modal.spec.js
@@ -1,1 +1,11 @@
-// modal spec tests
+import Modal from "./modal"
+import Undernet from "../../src/undernet"
+
+global.window.Undernet = Undernet
+
+describe("<Modal />", () => {
+  it("renders", () => {
+    const wrapper = mount(<Modal />)
+    expect(wrapper).to.have.length(1)
+  })
+})
