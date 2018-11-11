2949a768d319e343626e491c78b6af769750d6c3
george <george@scribd.com>
2018-11-11 13:27:12 -0800

    adds some working tests using basic node assertions with jsdom-global

--- js/test/modal/modal.spec.js
+++ js/test/modal/modal.spec.js
@@ -1,11 +1,42 @@
-import Modal from "./modal"
+import assert from "assert"
 import Undernet from "../../src/undernet"
 
-global.window.Undernet = Undernet
+document.body.innerHTML = `
+    <button href="#" data-modal-button data-target="new-modal">Open modal</button>
+    
+    <div className="modal-overlay" data-modal-id="new-modal">
+      <div className="modal-dialog" data-parent="new-modal" aria-labelledby="header-id" data-modal>
+        <header>
+          <h2 className="h6 has-no-margin-top" id="header-id">
+            Modal Header
+          </h2>
+          <a data-close href="#">
+            <span aria-hidden="true">&times;</span>
+          </a>
+        </header>
+        <section>
+          <p>Some modal content here</p>
+        </section>
+        <footer>
+          <a className="button" data-close href="#">
+            Cancel
+          </a>
+          <a className="primary button" href="#">
+            OK
+          </a>
+        </footer>
+      </div>
+    </div>
+  `
 
-describe("<Modal />", () => {
-  it("renders", () => {
-    const wrapper = mount(<Modal />)
-    expect(wrapper).to.have.length(1)
+describe("Modals", () => {
+  it("renders with [role='dialog']", () => {
+    Undernet.Modals.start()
+    assert.strictEqual("dialog", document.querySelector("[data-modal]").getAttribute("role"))
+  })
+
+  it("renders with [aria-modal='true']", () => {
+    Undernet.Modals.start()
+    assert.strictEqual("true", document.querySelector("[data-modal]").getAttribute("aria-modal"))
   })
 })
