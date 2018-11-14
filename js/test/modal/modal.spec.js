e0efa3ea40820834a5a214e974d4b2159b52cde3
george <george@scribd.com>
2018-11-14 11:00:16 -0800

    test cleanup

--- js/test/modal/modal.spec.js
+++ js/test/modal/modal.spec.js
@@ -27,7 +27,7 @@ const dom = `
 `
 
 describe("Modals", function() {
-  describe("#start", function() {
+  describe("API #start", function() {
     let modalOverlay
     let modalDialog
 
@@ -45,7 +45,7 @@ describe("Modals", function() {
     it("adds [data-visible='false'] to modal overlay", function() {
       expect(modalOverlay.getAttribute("data-visible")).to.equal("false")
     })
-      
+
     it("adds [role='dialog'] to modal dialog", function() {
       expect(modalDialog.getAttribute("role")).to.equal("dialog")
     })
@@ -54,7 +54,7 @@ describe("Modals", function() {
       expect(modalDialog.getAttribute("aria-modal")).to.equal("true")
     })
 
-    it(`adds [tabindex='-1'] to each focusable element within the modal dialog`, function() {
+    it(`adds [tabindex='-1'] to each focusable element within modal dialog`, function() {
       const focusableElements = modalDialog.querySelectorAll("a")
       focusableElements.forEach(el => {
         expect(el.getAttribute("tabindex")).to.equal("-1")
@@ -62,7 +62,7 @@ describe("Modals", function() {
     })
   })
 
-  describe("#stop", function() {
+  describe("API #stop", function() {
     let button
     let modalOverlay
     let modalDialog
@@ -77,19 +77,19 @@ describe("Modals", function() {
       button.click()
     })
 
-    it("does not modify [tabindex] on modal dialog when modal button is clicked", function() {
+    it("does not modify [tabindex] on modal dialog", function() {
       expect(modalDialog.getAttribute("tabindex")).to.equal(null)
     })
 
-    it("does not modify [data-visible] on modal overlay when modal button is clicked", function() {
+    it("does not modify [data-visible] on modal overlay", function() {
       expect(modalOverlay.getAttribute("data-visible")).to.equal("false")
     })
 
-    it("does not modify [aria-hidden] on modal overlay when modal button is clicked", function() {
+    it("does not modify [aria-hidden] on modal overlay", function() {
       expect(modalOverlay.getAttribute("aria-hidden")).to.equal("true")
     })
 
-    it("does not modify [tabindex] on each focusable element within the modal dialog", function() {
+    it("does not modify [tabindex] on each focusable element within modal dialog", function() {
       const focusableElements = modalDialog.querySelectorAll("a")
       focusableElements.forEach(el => {
         expect(el.getAttribute("tabindex")).to.equal("-1")
@@ -97,7 +97,7 @@ describe("Modals", function() {
     })
   })
 
-  describe("#_render", function() {
+  describe("#_render -> Modal Button Click", function() {
     let button
     let modalOverlay
     let modalDialog
@@ -111,19 +111,19 @@ describe("Modals", function() {
       button.click()
     })
 
-    it("sets [tabindex='-1'] on modal dialog when modal button is clicked", function() {
+    it("sets [tabindex='-1'] on modal dialog", function() {
       expect(modalDialog.getAttribute("tabindex")).to.equal("-1")
     })
 
-    it("sets [data-visible='true'] on modal overlay when modal button is clicked", function() {
+    it("sets [data-visible='true'] on modal overlay", function() {
       expect(modalOverlay.getAttribute("data-visible")).to.equal("true")
     })
 
-    it("sets [aria-hidden='false'] on modal overlay when modal button is clicked", function() {
+    it("sets [aria-hidden='false'] on modal overlay", function() {
       expect(modalOverlay.getAttribute("aria-hidden")).to.equal("false")
     })
 
-    it("adds [tabindex='0'] to each focusable element within the modal dialog", function() {
+    it("adds [tabindex='0'] to each focusable element within modal dialog", function() {
       const focusableElements = modalDialog.querySelectorAll("a")
       focusableElements.forEach(el => {
         expect(el.getAttribute("tabindex")).to.equal("0")
