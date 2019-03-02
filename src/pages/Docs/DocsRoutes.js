0cadb61aaf8c41b814247685a14887068d45e679
George Treviranus <geotrev@icloud.com>
2019-03-02 11:20:22 -0800

    removes html wrappers from DocsRoutes and reduces extra wrappers

--- src/pages/Docs/DocsRoutes.js
+++ src/pages/Docs/DocsRoutes.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Fragment } from "react"
 import { Route } from "react-router-dom"
 import Loadable from "react-loadable"
 import { Link } from "react-router-dom"
@@ -37,29 +37,25 @@ function getRoute(name) {
 
 export default function DocsRoutes() {
   return (
-    <div id="docs-routes" className="small-section fluid grid">
-      <div className="articles-wrapper row">
-        <div className="column">
-          <Route exact path={introductionPath} component={getRoute("introduction")} />
-          <Route exact path={downloadPath} component={getRoute("download")} />
-          <Route exact path={brandingPath} component={getRoute("branding")} />
-          <Route exact path={javascriptPath} component={getRoute("javascript")} />
-          <Route exact path={accessibilityPath} component={getRoute("accessibility")} />
-          <Route exact path={gridPath} component={getRoute("grid")} />
-          <Route exact path={typographyPath} component={getRoute("typography")} />
-          <Route exact path={buttonsPath} component={getRoute("buttons")} />
-          <Route exact path={formsPath} component={getRoute("forms")} />
-          <Route exact path={modalsPath} component={getRoute("modals")} />
-          <Route exact path={accordionsPath} component={getRoute("accordions")} />
-          <Route exact path={dropdownsPath} component={getRoute("dropdowns")} />
-          <Route exact path={alignmentPath} component={getRoute("alignment")} />
-          <Route exact path={offsetOrderPath} component={getRoute("offset-order")} />
-          <Route exact path={textPath} component={getRoute("text")} />
-          <Route exact path={displayPath} component={getRoute("display")} />
-          <Route exact path={colorPath} component={getRoute("color")} />
-          <Route exact path={spacingPath} component={getRoute("spacing")} />
-        </div>
-      </div>
-    </div>
+    <Fragment>
+      <Route exact path={introductionPath} component={getRoute("introduction")} />
+      <Route exact path={downloadPath} component={getRoute("download")} />
+      <Route exact path={brandingPath} component={getRoute("branding")} />
+      <Route exact path={javascriptPath} component={getRoute("javascript")} />
+      <Route exact path={accessibilityPath} component={getRoute("accessibility")} />
+      <Route exact path={gridPath} component={getRoute("grid")} />
+      <Route exact path={typographyPath} component={getRoute("typography")} />
+      <Route exact path={buttonsPath} component={getRoute("buttons")} />
+      <Route exact path={formsPath} component={getRoute("forms")} />
+      <Route exact path={modalsPath} component={getRoute("modals")} />
+      <Route exact path={accordionsPath} component={getRoute("accordions")} />
+      <Route exact path={dropdownsPath} component={getRoute("dropdowns")} />
+      <Route exact path={alignmentPath} component={getRoute("alignment")} />
+      <Route exact path={offsetOrderPath} component={getRoute("offset-order")} />
+      <Route exact path={textPath} component={getRoute("text")} />
+      <Route exact path={displayPath} component={getRoute("display")} />
+      <Route exact path={colorPath} component={getRoute("color")} />
+      <Route exact path={spacingPath} component={getRoute("spacing")} />
+    </Fragment>
   )
 }
