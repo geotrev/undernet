0773910c07a5b48254f7bcf1f9b321d00767b504
george <hello@geotrev.com>
2019-04-04 20:46:42 -0700

    add jest, begin converting all the tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    mocha: true,
+    jest: true,
     node: true,
     commonjs: true,
     es6: true,
--- site/components/SetMeta/SetMeta.spec.js
+++ site/components/SetMeta/SetMeta.spec.js
@@ -1,30 +1,9 @@
 import React from "react"
 import SetMeta from "./SetMeta"
-import { Helmet } from "react-helmet"
 
 describe("<SetMeta />", () => {
-  let wrapper
-  before(() => {
-    wrapper = mount(<SetMeta title="Test title" description="Test description" />)
-  })
-
-  it("renders", () => {
-    expect(wrapper).to.exist
-  })
-
-  it("renders <Helmet />", () => {
-    expect(wrapper)
-      .to.have.exactly(1)
-      .descendants(Helmet)
-  })
-
-  it("sets title on document", () => {
-    const element = document.querySelector("title")
-    expect(element.innerHTML).to.equal("Undernet – Test title")
-  })
-
-  it("sets description meta on document", () => {
-    const element = document.querySelector("meta[name='description']")
-    expect(element.getAttribute("content")).to.equal("Test description")
+  it("matches snapshot", () => {
+    const wrapper = mount(<SetMeta title="Test title" description="Test description" />)
+    expect(wrapper).toMatchSnapshot()
   })
 })
