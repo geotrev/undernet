5ec16ca7856332e5ddbfc8dffbcf256781aa9c67
george <hello@geotrev.com>
2019-04-09 19:54:53 -0700

    adds jsx-a11y and custom ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+const ERROR = "error"
+const READONLY = "readonly"
+
 module.exports = {
   env: {
     browser: true,
@@ -17,33 +20,55 @@ module.exports = {
       configFile: "babel.config.js",
     },
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "prettier",
-    "prettier/react",
-    "prettier/babel",
-  ],
+  extends: ["plugin:react/recommended", "prettier", "prettier/react", "prettier/babel", "jsx-a11y"],
   plugins: ["react"],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: READONLY,
+    SharedArrayBuffer: READONLY,
 
     // framework
     Undernet: "writable",
 
     // tests
-    should: "readonly",
-    expect: "readonly",
-    mount: "readyonly",
-    shallow: "readyonly",
-    chai: "readonly",
+    should: READONLY,
+    expect: READONLY,
+    mount: READONLY,
+    shallow: READONLY,
+    chai: READONLY,
   },
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
+    "for-direction": ERROR,
+    "getter-return": ERROR,
+    "no-async-promise-executor": ERROR,
+    "no-await-in-loop": ERROR,
+    "no-compare-neg-zero": ERROR,
+    "no-cond-assign": [ERROR, "always"],
+    "no-constant-condition": [ERROR, { checkLoops: true }],
+    "no-debugger": ERROR,
+    "no-dupe-args": ERROR,
+    "no-dupe-keys": ERROR,
+    "no-duplicate-case": ERROR,
+    "no-ex-assign": ERROR,
+    "no-extra-boolean-cast": ERROR,
+    "no-func-assign": ERROR,
+    "no-inner-declarations": ERROR,
+    "no-obj-calls": ERROR,
+    "no-irregular-whitespace": ERROR,
+    "no-misleading-character-class": ERROR,
+    "no-sparse-arrays": ERROR,
+    "no-prototype-builtins": ERROR,
+    "no-unexpected-multiline": ERROR,
+    "no-unreachable": ERROR,
+    "no-unsafe-finally": ERROR,
+    "no-unsafe-negation": ERROR,
+    "valid-typeof": ERROR,
+    "use-isnan": ERROR,
+    "require-atomic-updates": ERROR,
+
+    // code style and formatting
+    indent: [ERROR, 2],
+    quotes: [ERROR, "double"],
+    semi: [ERROR, "never"],
     "no-console": 0,
   },
 }
