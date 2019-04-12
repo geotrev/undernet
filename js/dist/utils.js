75c8777bb7259dd73ed98144a2df94249998d41b
george <hello@geotrev.com>
2019-04-11 20:54:52 -0700

    update js dist

--- js/dist/utils.js
+++ js/dist/utils.js
@@ -11,6 +11,14 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 var KeyCodes = {
   SHIFT: 16,
   TAB: 9,
@@ -28,7 +36,7 @@ var Events = {
 };
 
 var nodeListToArray = function nodeListToArray(nodeList) {
-  return Array.apply(null, document.querySelectorAll(nodeList));
+  return _toConsumableArray(document.querySelectorAll(nodeList));
 };
 
 exports.nodeListToArray = nodeListToArray;
