da810cbf4594989aeadb88862483b2334784213d
george <hello@geotrev.com>
2019-05-02 20:38:34 -0700

    update tests

--- site/components/PageNotFound/PageNotFound.spec.js
+++ site/components/PageNotFound/PageNotFound.spec.js
@@ -10,14 +10,4 @@ describe("<PageNotFound />", () => {
   it("matches snapshot", () => {
     expect(wrapper).toMatchSnapshot()
   })
-
-  it("renders h1 text", () => {
-    expect(wrapper.find("h1").text()).toEqual("Sorry, that page doesn't exist. :(")
-  })
-
-  it("renders paragraph text", () => {
-    expect(wrapper.find("p").text()).toEqual(
-      "Use the links up in the navigation bar to find your way around the site!"
-    )
-  })
 })
