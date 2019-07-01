aef6f7d5f9747522b5bd15dbb6afbc3821f9529e
george <geotrev@icloud.com>
2019-06-30 19:07:05 -0700

    update webpack build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,21 +1,12 @@
 const merge = require("webpack-merge")
 const CompressionPlugin = require("compression-webpack-plugin")
 const common = require("../webpack.common.js")
-const CleanWebpackPlugin = require("clean-webpack-plugin")
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 module.exports = merge(common, {
   optimization: {
     splitChunks: { chunks: "all" },
-    minimizer: [
-      new UglifyJsPlugin({
-        exclude: /\/node_modules/,
-        cache: true,
-        parallel: true,
-        sourceMap: false,
-      }),
-    ],
     runtimeChunk: {
       name: "manifest",
     },
