bf3919285f5cad7a26edb217af2820d84226eb56
george <geotrev@icloud.com>
2019-08-24 20:45:52 -0700

    mock package version and description fields in test to shorten publish time + update npmignore

--- app/components/SideNav/__tests__/SideNav.spec.js
+++ app/components/SideNav/__tests__/SideNav.spec.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router } from "react-router-dom"
 
 jest.mock("react-feather/dist/icons/chevron-right", () => global.simpleMock("ChevronRight"))
 jest.mock("react-feather/dist/icons/menu", () => global.simpleMock("Menu"))
+jest.mock("projectRoot/package.json", () => ({
+  version: "9.9.9",
+}))
 
 const MENU_COLLAPSE_WIDTH = 1199
 const MENU_EXPAND_WIDTH = MENU_COLLAPSE_WIDTH + 1
