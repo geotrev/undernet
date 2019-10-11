0934aad734721c5e01d8ffe29ec92f1db0a1f1c2
george <hello@geotrev.com>
2019-10-10 19:30:45 -0700

    adds the spec file and shim for jest

---
+++ src/__tests__/scss.spec.js
@@ -0,0 +1,9 @@
+const path = require("path")
+const sassTrue = require("sass-true")
+const glob = require("glob")
+
+describe("Scss", () => {
+  const sassTestFiles = glob.sync(path.resolve(process.cwd(), "src/**/*.spec.scss"))
+
+  sassTestFiles.forEach(file => sassTrue.runSass({ file }, { describe, it }))
+})
