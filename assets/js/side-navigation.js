197625eadd8befc5adb4d9f1b9bd810b4edd8a36
george <hello@geotrev.com>
2019-12-31 17:50:25 -0800

    remove generated scripts from revision history

--- assets/js/side-navigation.js
+++
@@ -1,2 +0,0 @@
-!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";document.addEventListener("DOMContentLoaded",(function(){console.log("Hello world from Jekyll scripts!")}))}));
-//# sourceMappingURL=side-navigation.js.map
